5fbb1e12ba35ac981d9307b76f569471876b4cfb
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2021-11-02 14:30:11 +0100

    First working version

--- diskspace/main.swift
+++ diskspace/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
-
+let fileURL = URL(fileURLWithPath:"/")
+do {
+    let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityKey,.volumeAvailableCapacityForImportantUsageKey, .volumeAvailableCapacityForOpportunisticUsageKey, .volumeTotalCapacityKey])
+    if let importantCapacity = values.volumeAvailableCapacity {
+        print("Available:     \(importantCapacity)")
+    }
+    if let importantCapacity = values.volumeAvailableCapacityForImportantUsage {
+        print("Important:     \(importantCapacity)")
+    }
+    if let opportunisticCapacity = values.volumeAvailableCapacityForOpportunisticUsage {
+        print("Opportunistic: \(opportunisticCapacity)")
+    }
+    if let totalCapacity = values.volumeTotalCapacity {
+        print("Total:         \(totalCapacity)")
+    }
+} catch {
+    print("Error retrieving capacity: \(error.localizedDescription)")
+}
