057605dbfeff1844881bda2d7a812df71d67b937
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2021-11-02 15:03:05 +0100

    added argument-parser and a human-readable flag

--- diskspace/main.swift
+++ diskspace/main.swift
@@ -6,22 +6,47 @@
 //
 
 import Foundation
+import ArgumentParser
 
-let fileURL = URL(fileURLWithPath:"/")
-do {
-    let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityKey,.volumeAvailableCapacityForImportantUsageKey, .volumeAvailableCapacityForOpportunisticUsageKey, .volumeTotalCapacityKey])
-    if let importantCapacity = values.volumeAvailableCapacity {
-        print("Available:     \(importantCapacity)")
+// Code based on sample from here:
+// https://developer.apple.com/documentation/foundation/urlresourcekey/checking_volume_storage_capacity
+
+struct DiskSpace : ParsableCommand {
+    @Flag var humanReadable: Bool = false
+    
+    
+    func format(_ int: Int) -> String {
+        return format(Int64(int))
     }
-    if let importantCapacity = values.volumeAvailableCapacityForImportantUsage {
-        print("Important:     \(importantCapacity)")
+    
+    func format(_ int: Int64) -> String {
+        if humanReadable {
+            return ByteCountFormatter().string(fromByteCount: Int64(int))
+        } else {
+            return String(int)
+        }
     }
-    if let opportunisticCapacity = values.volumeAvailableCapacityForOpportunisticUsage {
-        print("Opportunistic: \(opportunisticCapacity)")
+    
+    func run() {
+        let systemVolume = URL(fileURLWithPath:"/")
+        do {
+            let values = try systemVolume.resourceValues(forKeys: [.volumeAvailableCapacityKey,.volumeAvailableCapacityForImportantUsageKey, .volumeAvailableCapacityForOpportunisticUsageKey, .volumeTotalCapacityKey])
+            if let totalCapacity = values.volumeAvailableCapacity {
+                print("Available:     \(format(totalCapacity))")
+            }
+            if let importantCapacity = values.volumeAvailableCapacityForImportantUsage {
+                print("Important:     \(format(importantCapacity))")
+            }
+            if let opportunisticCapacity = values.volumeAvailableCapacityForOpportunisticUsage {
+                print("Opportunistic: \(format(opportunisticCapacity))")
+            }
+            if let totalCapacity = values.volumeTotalCapacity {
+                print("Total:         \(format(totalCapacity))")
+            }
+        } catch {
+            print("Error retrieving capacity: \(error.localizedDescription)")
+        }
     }
-    if let totalCapacity = values.volumeTotalCapacity {
-        print("Total:         \(totalCapacity)")
-    }
-} catch {
-    print("Error retrieving capacity: \(error.localizedDescription)")
 }
+
+DiskSpace.main()
